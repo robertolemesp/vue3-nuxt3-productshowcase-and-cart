06815e2652dfb0c7ad5e21c70617411a1c622df1
Roberto <robertolemesp@hotmail.com>
2022-10-09 18:06:27 -0300

    Updates app to render home template and fetch product data and store

--- app.vue
+++ app.vue
@@ -1,4 +1,7 @@
 <script lang='ts' setup>
+  import type { Product } from '@/models/product'
+  import { getProducts } from '@/services/product'
+
   useHead({
     title: 'Code Challenge',
     viewport: 'width=device-witdh, initial-scale=1, maximum-scale=1',
@@ -7,7 +10,14 @@
       { name: 'description', content: 'Consulta Remedios Frontend Challenge Roberto' }
     ]
   })
+
+  const { data: products/*,  pending */ } = await getProducts()
+
+  useProductState().setProducts(products as unknown as Product[])
 </script>
 <template>
-  <div />
+  <NuxtLayout>
+    <NuxtPage />
+    <TemplateHome />
+  </NuxtLayout>
 </template>
