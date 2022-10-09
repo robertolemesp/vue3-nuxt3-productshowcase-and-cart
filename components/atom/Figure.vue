49a0a305f761359bdec965b26f087cc4728f5f9f
Roberto <robertolemesp@hotmail.com>
2022-10-09 18:09:43 -0300

    Adds Figure and Heading Atoms

---
+++ components/atom/Figure.vue
@@ -0,0 +1,24 @@
+<script lang='ts' setup>
+  interface Props {
+    imageAlt?: string
+    imageSrc: string
+  }
+
+  const { imageAlt = '', imageSrc = '' } = defineProps<Props>()
+</script>
+<template>
+  <figure>
+    <img :src="`assets/images/${imageSrc}`" :alt="imageAlt">
+  </figure>
+</template>
+<style lang="scss" scoped>
+  figure {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: $gray;
+    border-radius: 3px;
+
+    img { width: 65%; }
+  }
+</style>
---
+++ components/atom/Heading.vue
@@ -0,0 +1,24 @@
+<script lang='ts' setup>
+  interface Props {
+    variant?: 'big' | 'small'
+  }
+
+  const { variant = 'big' } = defineProps<Props>()
+
+  const computedStyle = computed(() =>({
+    fontSize: `${variant === 'big' ? 48 : 18}px`,
+    fontFamily: `Open Sans ${variant === 'big' ? 'Semi-' : ''}Bold, sans-serif`
+  }))
+
+</script>
+<template>
+  <h1 :style="computedStyle">
+    <slot />
+  </h1>
+</template>
+<style lang="scss" scoped>
+  h1 {
+    letter-spacing: 0;
+    color: $gray-6;
+  }
+</style>
