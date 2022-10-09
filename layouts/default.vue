fc42cb6ac903e0b3e294b52413a639083440aef2
Roberto <robertolemesp@hotmail.com>
2022-10-09 17:57:02 -0300

    Adds app layout

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <slot />
+  </main>
+</template>
+<style lang="scss" scoped>
+  main {
+    display: flex;
+    flex-flow: row wrap;
+    width: 100%;
+    max-width: 1280px;
+    margin: 42px auto;
+    padding: 0 70px;
+  }
+</style>
